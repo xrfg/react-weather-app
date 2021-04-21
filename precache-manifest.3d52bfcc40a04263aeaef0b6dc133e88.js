0ed37481ee94d70610c1a71ddb6f96fcaf5491cb
henriquemartins <henrm@riseup.net>
2021-04-21 11:18:48 +0200

    Updates

---
+++ precache-manifest.3d52bfcc40a04263aeaef0b6dc133e88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d61c6ac3dfbdddb18c86347226de4e2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9d8d75f67fd8c880d217",
+    "url": "./static/js/2.7ac8a546.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.7ac8a546.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6b3c6169d7392b200ef",
+    "url": "./static/js/main.8c686d03.chunk.js"
+  },
+  {
+    "revision": "8cdb25f3ffb41440c4d8",
+    "url": "./static/js/runtime-main.e6c4d886.js"
+  }
+]);
